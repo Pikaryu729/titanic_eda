cae76dadbf25d51f31cad41a13d4e195f4ee6322
Pikaryu729 <ryushin.wells@gmail.com>
2025-06-30 16:28:26 -0400

    added deployment scripts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   /* config options here */
+  basePath: isProd ? "/titanic-eda" : "",
+  assetPrefix: isProd ? "/titanic-eda" : "",
 };
 
 export default nextConfig;
