583b0439fb341abbe56cd03c5dbbba4ab64db610
Ryushin Wells <ryushin.wells@gmail.com>
2025-06-30 18:11:59 -0400

    idek

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig: NextConfig = {
   output: "export",
   /* config options here */
-  basePath: isProd ? "/titanic-eda" : "",
-  assetPrefix: isProd ? "/titanic-eda/" : "",
+  basePath: "/titanic-eda",
+  assetPrefix: "/titanic-eda/",
 };
 
 export default nextConfig;
