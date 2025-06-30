247f79a0553ffae80c419cd23045ebdde7024ded
Ryushin Wells <ryushin.wells@gmail.com>
2025-06-30 19:00:42 -0400

    removed static building

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 
 const nextConfig: NextConfig = {
-  output: "export",
   /* config options here */
-  basePath: "/titanic-eda",
-  assetPrefix: "/titanic-eda/",
 };
 
 export default nextConfig;
